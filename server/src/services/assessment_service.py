685908ab02b18ca596e21e7fb84e6f7de57bb1fa
Tapabrata Chowdhury <tapabratachowdhury12@gmail.com>
2026-04-29 20:14:11 +0530

    feat:Did some slight changes and added requirements.txt

--- server/src/services/assessment_service.py
+++ server/src/services/assessment_service.py
@@ -1,10 +1,11 @@
 # File: server/src/services/assessment_service.py
 from typing import Tuple
-from src.schemas.assessment import PHQ9Severity
+from src.schemas.assessment import PHQ9Severity,PHQ9AssessmentRequest
 def _determine_phq9_severity(total_score:int)->PHQ9Severity:
     """
     :param total_score: int
     :return String: PHQ9Severity
+    :Workings: It takes the total PHQ-9 score and returns message according to that score
     """
     if 0 <= total_score <= 4:
         return PHQ9Severity("minimal")
@@ -19,3 +20,6 @@ def _determine_phq9_severity(total_score:int)->PHQ9Severity:
         return PHQ9Severity("moderately_severe")
 
     return PHQ9Severity("severe")
+
+def _evaluate_follow_up_flags(question:PHQ9AssessmentRequest):
+    pass
